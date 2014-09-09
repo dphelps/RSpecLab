8d603e8fd797c58354b7e195388d15449ba3e268
Daniel Phelps <dphelps@cmu.edu>
2014-09-09 15:23:28 +0300

    Update Movie spec with before :each

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -1,25 +1,25 @@
 require 'spec_helper'
 
 describe "Movie" do 
-	it "Instantiates a generic Movie Object" do
+	before :each do
 		@movie = Movie.new
+	end
+
+	it "Instantiates a generic Movie Object" do
 		expect(@movie).to be_instance_of Movie
 	end	
 
 	it "should have setter and getter methods for title" do
-		@movie = Movie.new
 		@movie.title = "Title"
 		expect(@movie.title).to eq 'Title'
 	end
 
 	it "should have setter and getter methods for rating" do
-		@movie = Movie.new
 		@movie.rating = "R"
 		expect(@movie.rating).to eq 'R'
 	end
 
 	it "should have setter and getter methods for runtime" do
-		@movie = Movie.new
 		@movie.runtime = 120
 		expect(@movie.runtime).to eq 120
 	end
